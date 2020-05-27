385a89d83df9c076963d2943507d1527ffa606f7
Rodrigo Campos <rodrigo@kinvolk.io>
2020-05-27 16:37:39 -0300

    Add manual tests and their output
    This commits adds some test examples and their output when running, to
    easily see the current behavior.
    
    They use a hostPath to write to a local file. To run it locally, you can
    just:
    
    	mkdir /tmp/logs/
    	cp sidecar-tests/logs.sh /tmp/logs/
    	kubectl apply -f $test
    	cat /tmp/logs/log.txt
    	rm /tmp/logs/log.txt
    
    replacing $test with the yaml file you want to run.

---
+++ sidecar-tests/logs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+DATE=$(date)
+CONTAINERID=$(basename $(cat proc/self/cpuset) | cut -c 1-12)
+LOG="/logs/log.txt"
+
+case $1 in
+sleep)
+	trap "echo $DATE $NAME $CONTAINERID Received SIGTERM >> $LOG; exit 0" SIGTERM
+	echo "$DATE $NAME $CONTAINERID Starting" >> $LOG
+
+	sleep 10
+	touch /tmp/ready
+
+	while true
+	do
+		sleep 1
+	done
+	;;
+*)
+	echo "$DATE $NAME $CONTAINERID $*" >> $LOG
+	;;
+esac
