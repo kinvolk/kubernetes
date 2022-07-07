c8b36241151df3c3d088a86b64faabf522e30489
Giuseppe Scrivano <gscrivan@redhat.com>
2022-07-07 11:08:32 +0200

    features: add UserNamespacesSupport feature
    define a feature gate for the user namespaces support.  The feature is
    not enabled by default.
    
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -801,6 +801,12 @@ const (
 	// Allow users to specify whether to take nodeAffinity/nodeTaint into consideration when
 	// calculating pod topology spread skew.
 	NodeInclusionPolicyInPodTopologySpread featuregate.Feature = "NodeInclusionPolicyInPodTopologySpread"
+
+	// owner: @rata, @giuseppe
+	// kep: http://kep.k8s.io/127
+	//
+	// Enables user namespace support.
+	UserNamespacesSupport featuregate.Feature = "UserNamespacesSupport"
 )
 
 func init() {
@@ -1052,4 +1058,6 @@ var defaultKubernetesFeatureGates = map[featuregate.Feature]featuregate.FeatureS
 	// features that enable backwards compatibility but are scheduled to be removed
 	// ...
 	HPAScaleToZero: {Default: false, PreRelease: featuregate.Alpha},
+
+	UserNamespacesSupport: {Default: false, PreRelease: featuregate.Alpha},
 }
